ae603a14d3c7358481b94e7ba17b3805a2048ef4
Eugenia Tuffano <54639432+EugeTuffano@users.noreply.github.com>
2022-03-04 14:53:07 -0300

    Primer commit

---
+++ FrmEmployee.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BLL;
+using DAL;
+using DAL.DTO;
+using System.IO;
+
+namespace PersonalTracking
+{
+    public partial class FrmEmployee : Form
+    {
+        public FrmEmployee()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtUserNo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = General.isNumber(e);
+        }
+
+        private void txtSalary_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtSalary_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = General.isNumber(e);
+        }
+
+        EmployeeDTO dto = new EmployeeDTO();
+        private void FrmEmployee_Load(object sender, EventArgs e)
+        {
+            dto = EmployeeBLL.GetAll();
+            cmbDepartment.DataSource = dto.Departments;
+            cmbDepartment.DisplayMember = "DeparmentName";
+            cmbDepartment.ValueMember = "ID";
+            cmbPosition.DataSource = dto.Positions;
+            cmbPosition.DisplayMember = "PositionName";
+            cmbPosition.ValueMember = "ID";
+            cmbDepartment.SelectedIndex = -1;
+            cmbPosition.SelectedIndex = -1;
+            combofull = true;
+        }
+
+        bool combofull = false;
+
+        private void cmbDepartment_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (combofull)
+            {
+                int departmentID = Convert.ToInt32(cmbDepartment.SelectedValue);
+                cmbPosition.DataSource = dto.Positions.Where(x => x.DepartmentID == departmentID).ToList();
+            }
+            
+
+        }
+
+        string fileName = "";
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+                txtImagePath.Text = openFileDialog1.FileName;
+                string Unique = Guid.NewGuid().ToString();
+                fileName += Unique + openFileDialog1.SafeFileName;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtUserNo.Text.Trim() == "")
+                MessageBox.Show("User number is empty");
+            else if (!EmployeeBLL.isUnique(Convert.ToInt32(txtUserNo.Text)))
+                MessageBox.Show("This user number is used by another employee, please change");
+            else if (txtPassword.Text.Trim() == "")
+                MessageBox.Show("Password is empty");
+            else if (txtName.Text.Trim() == "")
+                MessageBox.Show("Name is empty");
+            else if (txtSurname.Text.Trim() == "")
+                MessageBox.Show("Surname is empty");
+            else if (txtSalary.Text.Trim() == "")
+                MessageBox.Show("Salary is empty");
+            else if (cmbDepartment.SelectedIndex == -1)
+                MessageBox.Show("Select a department");
+            else if (cmbPosition.SelectedIndex == -1)
+                MessageBox.Show("Select a position");
+            else
+            {
+                Employee employee = new Employee();
+                employee.UserNo = Convert.ToInt32(txtUserNo.Text);
+                employee.Password = txtPassword.Text;
+                employee.isAdmin = chAdmin.Checked;
+                employee.Name = txtName.Text;
+                employee.Surname = txtSurname.Text;
+                employee.Salary = Convert.ToInt32(txtSalary.Text);
+                employee.DepartmentID = Convert.ToInt32(cmbDepartment.SelectedValue);
+                employee.PositionID = Convert.ToInt32(cmbPosition.SelectedValue);
+                employee.BirthDay = dateTimePicker1.Value;
+                employee.Adress = txtAdress.Text;
+                employee.ImagePath = fileName;
+
+                EmployeeBLL.AddEmployee(employee);
+                File.Copy(txtImagePath.Text, @"images\\" + fileName);
+                MessageBox.Show("Employee was added");
+
+                txtUserNo.Clear();
+                txtPassword.Clear();
+                chAdmin.Checked = false;
+                txtName.Clear();
+                txtSurname.Clear();
+                txtSalary.Clear();
+                cmbDepartment.SelectedIndex = -1;
+                cmbPosition.DataSource = dto.Positions;
+                cmbPosition.SelectedIndex = -1;
+                combofull = true;
+                dateTimePicker1.Value = DateTime.Today;
+                txtAdress.Clear();
+                txtImagePath.Clear();
+                pictureBox1.Image = null;
+            }
+        }
+
+        bool isUnique = false;
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            if (txtUserNo.Text.Trim() == "")
+                MessageBox.Show("User number is empty");
+            else
+            {
+                isUnique = EmployeeBLL.isUnique(Convert.ToInt32(txtUserNo.Text));
+                if (!isUnique)
+                    MessageBox.Show("This user number is used by another employee, please change");
+                else
+                    MessageBox.Show("This user number is usable");
+            }
+        }
+    }
+}
